119530cb35142fadb59ecb94bde3ede56dbd8b8a
Vladster1256 <vcmariorocks@gmail.com>
2014-11-12 12:42:33 -0700

    st

--- gen/ctec/samplestateproject/R.java
+++ gen/ctec/samplestateproject/R.java
@@ -1092,8 +1092,9 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050041;
         public static final int activity_chooser_view_content=0x7f050025;
+        public static final int ageField=0x7f05003e;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
@@ -1114,9 +1115,11 @@ containing a value of this type.
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
+        public static final int nameField=0x7f05003d;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int powerField=0x7f05003f;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1135,6 +1138,7 @@ containing a value of this type.
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
+        public static final int stuffsField=0x7f050040;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
--- src/ctec/samplestateproject/TransferDataActivity.java
+++ src/ctec/samplestateproject/TransferDataActivity.java
@@ -4,14 +4,31 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Application;
 
-public class TransferDataActivity extends Activity
+public class TransferDataActivity extends Application
 {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
+	private String name; 
+	private String age;
+	private String powerLevel;
+	private String stuffs;
+	
+	public void onCreate()
 	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_transfer_data);
+		super.onCreate();
+		
+		name = "";
+		age = "";
+		powerLevel = "";
+		stuffs = "";
+		
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(TransferDataActivity name)
+	{
+		this.name = name;
 	}
 }

db478687a89f6fddf4edc339b13abaf8faf59538
Vladster1256 <vcmariorocks@gmail.com>
2014-11-12 12:21:31 -0700

    STUFFS
    WHAT???

---
+++ src/ctec/model/samplestateproject/AndroidAppState.java
@@ -0,0 +1,19 @@
+package ctec.model.samplestateproject;
+
+import ctec.samplestateproject.R;
+import ctec.samplestateproject.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AndroidAppState extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_android_app_state);
+	}
+}
